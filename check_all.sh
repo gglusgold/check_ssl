df9281489a95dcd7509ce6134f5c8d0de5bdbe70
gglusgold <gabriel.glusgold@alu.unq.edu.ar>
2026-04-28 13:40:51 +0000

    news

---
+++ check_all.sh
@@ -0,0 +1,38 @@
+./check_ssl.sh asebien.com
+./check_ssl.sh asebien.com.ar
+"./check_ssl.sh asebienestar.com.ar	"
+./check_ssl.sh asenovedades.com.ar
+./check_ssl.sh camdoctor.ar
+./check_ssl.sh camdoctor.com
+./check_ssl.sh camdoctor.com.ar
+./check_ssl.sh cmbrown.com.ar
+./check_ssl.sh conectacontubienestar.com.ar
+"./check_ssl.sh farmaconecta.ar	"
+"./check_ssl.sh farmaconecta.com.ar	"
+./check_ssl.sh finochiettosalud.com.ar
+./check_ssl.sh fundacionmedifeinfo.com.ar
+"./check_ssl.sh gestionesmedife.com.ar	"
+"./check_ssl.sh mailingmedife.com.ar	"
+./check_ssl.sh medife.ar
+./check_ssl.sh medifeinfo.com.ar
+./check_ssl.sh medifemailing.com.ar
+./check_ssl.sh novedadesmedife.com.ar
+./check_ssl.sh opticaconecta.ar
+./check_ssl.sh opticaconecta.com.ar
+./check_ssl.sh osprogmed.ar
+./check_ssl.sh osprogmed.com.ar
+"./check_ssl.sh pagosmedife.com.ar	"
+./check_ssl.sh planpgn.com.ar
+./check_ssl.sh pmbeneficios.com.ar
+./check_ssl.sh pmmutualonline.com.ar
+"./check_ssl.sh prestadoresmedife.com.ar	"
+"./check_ssl.sh progmed.com.ar	"
+./check_ssl.sh programasmedicos.ar
+./check_ssl.sh programasmedicos.com.ar
+./check_ssl.sh programasmedicos.com.ar
+./check_ssl.sh sanatoriofinochietto.com.ar
+./check_ssl.sh sfinochietto.com.ar
+./check_ssl.sh somosase.com.ar
+"./check_ssl.sh tramitesmedife.com.ar	"
+./check_ssl.sh tubeneficio.com.ar
+./check_ssl.sh vivimedife.com.ar
